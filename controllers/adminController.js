9c5052987a1ff1d5de43c37cd82daa77dbdd6983
rizkisadewa <rizkykysadewa@gmail.com>
2018-05-22 07:39:21 +0800

    input class in adminController, still error in Authentication

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -3,204 +3,239 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
 
-//Bring in Models
-let Admin = require('../models/adminModel');
+class adminController {
+  constructor() {
+    this.importModel();
+    this.memasukkanAdmin();
+    this.mengubahAdmin();
+    this.menghapusAdmin();
+    this.melihatAdmin();
+    this.startAdmin();
+    this.login();
+    this.logout();
+    this.checkValidasi();
+  }
 
-// *** dashboard *** //
-//dashboard router
-router.get('/', ensureAuthenticated, (req, res)=>
-  res.render('admin/administrator/dashboard'));
+  importModel(){
+    //Bring in Models
+    let Admin = require('../models/adminModel');
+  }
 
+  startAdmin(){
+    // *** dashboard *** //
+    //dashboard router
+    router.get('/', ensureAuthenticated, (req, res)=>
+      res.render('admin/administrator/dashboard'));
+  }
 
-// *** data admin *** //
-// ** admin data view router
-router.get('/data', ensureAuthenticated, (req, res) => {
-  /*
-  // *** Testing JSON *** //
-  var admin = [
-    {
-      adminNo: "Admin 1",
-      name: "Santika",
-      username: "santika",
-      password: "***",
-      accessType: "super user"
-    },
-    {
-      adminNo: "Admin 2",
-      name: "Rizky Sadewa",
-      username: "rizky.sadewa",
-      password: "***",
-      accessType: "admin"
-    }
-  ];
-  res.render('admin/adminData', {
-    admin: admin
-  });
-  */
-  Admin.find({}, (err, admins)=>{
-    if(err){
-      console.log(err);
-    } else {
-      res.render('admin/administrator/adminData', {
-        admins: admins
+  melihatAdmin(){
+    // *** data admin *** //
+    // ** admin data view router
+    router.get('/data', ensureAuthenticated, (req, res) => {
+      /*
+      // *** Testing JSON *** //
+      var admin = [
+        {
+          adminNo: "Admin 1",
+          name: "Santika",
+          username: "santika",
+          password: "***",
+          accessType: "super user"
+        },
+        {
+          adminNo: "Admin 2",
+          name: "Rizky Sadewa",
+          username: "rizky.sadewa",
+          password: "***",
+          accessType: "admin"
+        }
+      ];
+      res.render('admin/adminData', {
+        admin: admin
       });
-    }
-  });
+      */
+      Admin.find({}, (err, admins)=>{
+        if(err){
+          console.log(err);
+        } else {
+          res.render('admin/administrator/adminData', {
+            admins: admins
+          });
+        }
+      });
 
-});
+    });
+  }
 
-// ** add admin data
-// router
-router.get('/add', ensureAuthenticated, (req, res)=>
-  res.render('admin/administrator/adminAdd'));
+  memasukkanAdmin(){
+    // ** add admin data
+    // router
+    router.get('/add', ensureAuthenticated, (req, res)=>
+      res.render('admin/administrator/adminAdd'));
 
-//Add submit POST route
-router.post('/add', (req, res)=>{
+    //Add submit POST route
+    router.post('/add', (req, res)=>{
 
-  let admin = new Admin();
-  admin.adminNo = req.body.adminNo;
-  admin.name = req.body.name;
-  admin.username = req.body.username;
-  admin.password = req.body.password;
-  admin.accessType = req.body.accessType;
+      let admin = new Admin();
+      admin.adminNo = req.body.adminNo;
+      admin.name = req.body.name;
+      admin.username = req.body.username;
+      admin.password = req.body.password;
+      admin.accessType = req.body.accessType;
 
-  admin.save(function(err){
-    if(err){
-      console.log(err);
-      return;
-    } else {
-      req.flash('success', 'Admin Added');
-      res.redirect('/admin/data');
-    }
-  });
+      admin.save(function(err){
+        if(err){
+          console.log(err);
+          return;
+        } else {
+          req.flash('success', 'Admin Added');
+          res.redirect('/admin/data');
+        }
+      });
 
-
-});
-
-// ** update admin
-// router of load edit form
-router.get('/edit/:id', ensureAuthenticated, (req, res)=>{
-  Admin.findById(req.params.id, function(err, admin){
-    res.render('admin/administrator/adminEdit',{
-      //title: 'Edit Admin',
-      admin: admin
 
     });
 
-  });
-});
+    // Admin Register
+    router.get('/register', (req, res)=>
+      res.render('admin/register'));
 
-//Update submit POST route
-router.post('/edit/:id', (req, res)=>{
+    //Register Process
+    router.post('/register', (req, res)=>{
 
-  let admin = {};
-  admin.adminNo = req.body.adminNo;
-  admin.name = req.body.name;
-  admin.username = req.body.username;
-  admin.password = req.body.password;
-  admin.accessType = req.body.accessType;
+      const adminNo = req.body.adminNo;
+      const name = req.body.name;
+      const username = req.body.username;
+      const password = req.body.password;
+      const accessType = req.body.accessType;
 
-  let query = {_id:req.params.id}
+      let newUser = new Admin({
+        adminNo: adminNo,
+        name: name,
+        username: username,
+        password: password,
+        accessType: accessType
+      });
 
-  Admin.update(query, admin, function(err){
-    if(err){
-      console.log(err);
-      return;
-    } else {
-      req.flash('success', 'Admin Updated');
-      res.redirect('/admin/data');
-    }
-  });
-
-});
-
-// Admin Delete
-router.delete('/data/:id', (req, res)=>{
-  let query = {_id:req.params.id}
-
-  Admin.remove(query, (err)=>{
-    if(err){
-      console.log(err);
-    }
-    req.flash('danger', 'Admin Deleted');
-    res.send('Success');
-
-  });
-});
-
-// Admin Register
-router.get('/register', (req, res)=>
-  res.render('admin/register'));
-
-//Register Process
-router.post('/register', (req, res)=>{
-
-  const adminNo = req.body.adminNo;
-  const name = req.body.name;
-  const username = req.body.username;
-  const password = req.body.password;
-  const accessType = req.body.accessType;
-
-  let newUser = new Admin({
-    adminNo: adminNo,
-    name: name,
-    username: username,
-    password: password,
-    accessType: accessType
-  });
-
-  //encrypted the Password
-  bcrypt.genSalt(10, (err, salt)=>{
-    bcrypt.hash(newUser.password, salt, (err, hash)=>{
-      if(err){
-        console.log(err);
-      } else {
-        newUser.password = hash;
-        newUser.save((err)=>{
+      //encrypted the Password
+      bcrypt.genSalt(10, (err, salt)=>{
+        bcrypt.hash(newUser.password, salt, (err, hash)=>{
           if(err){
             console.log(err);
-            return;
           } else {
-            req.flash('success', 'You are now registered and can log in');
-            res.redirect('/admin/login');
+            newUser.password = hash;
+            newUser.save((err)=>{
+              if(err){
+                console.log(err);
+                return;
+              } else {
+                req.flash('success', 'You are now registered and can log in');
+                res.redirect('/admin/login');
+              }
+            });
           }
         });
-      }
+      });
+
+
     });
-  });
 
-
-});
-
-// Login Form
-router.get('/login', (req, res)=>{
-  res.render('admin/login');
-});
-
-// Login Process
-router.post('/login', (req, res, next)=>{
-  passport.authenticate('local', {
-    successRedirect: '/admin',
-    failureRedirect: '/admin/login',
-    failureFlash: true
-  })(req, res, next);
-});
-
-// Logout
-router.get('/logout', (req, res)=>{
-  req.logout();
-  req.flash('success', 'You are logged out');
-  res.redirect('/admin/login');
-});
-
-// Access Control
-function ensureAuthenticated(req, res, next){
-  if (req.isAuthenticated()) {
-    return next();
-  } else {
-    req.flash('danger', 'Please Login as an Admin');
-    res.redirect('/admin/login');
   }
+
+  mengubahAdmin(){
+    // ** update admin
+    // router of load edit form
+    router.get('/edit/:id', ensureAuthenticated, (req, res)=>{
+      Admin.findById(req.params.id, function(err, admin){
+        res.render('admin/administrator/adminEdit',{
+          //title: 'Edit Admin',
+          admin: admin
+
+        });
+
+      });
+    });
+
+    //Update submit POST route
+    router.post('/edit/:id', (req, res)=>{
+
+      let admin = {};
+      admin.adminNo = req.body.adminNo;
+      admin.name = req.body.name;
+      admin.username = req.body.username;
+      admin.password = req.body.password;
+      admin.accessType = req.body.accessType;
+
+      let query = {_id:req.params.id}
+
+      Admin.update(query, admin, function(err){
+        if(err){
+          console.log(err);
+          return;
+        } else {
+          req.flash('success', 'Admin Updated');
+          res.redirect('/admin/data');
+        }
+      });
+
+    });
+  }
+
+  menghapusAdmin(){
+    // Admin Delete
+    router.delete('/data/:id', (req, res)=>{
+      let query = {_id:req.params.id}
+
+      Admin.remove(query, (err)=>{
+        if(err){
+          console.log(err);
+        }
+        req.flash('danger', 'Admin Deleted');
+        res.send('Success');
+
+      });
+    });
+  }
+
+  login(){
+    // Login Form
+    router.get('/login', (req, res)=>{
+      res.render('admin/login');
+    });
+
+    // Login Process
+    router.post('/login', (req, res, next)=>{
+      passport.authenticate('local', {
+        successRedirect: '/admin',
+        failureRedirect: '/admin/login',
+        failureFlash: true
+      })(req, res, next);
+    });
+  }
+
+  logout(){
+    // Logout
+    router.get('/logout', (req, res)=>{
+      req.logout();
+      req.flash('success', 'You are logged out');
+      res.redirect('/admin/login');
+    });
+  }
+
+  checkValidasi(){
+    // Access Control
+    function ensureAuthenticated(req, res, next){
+      if (req.isAuthenticated()) {
+        return next();
+      } else {
+        req.flash('danger', 'Please Login as an Admin');
+        res.redirect('/admin/login');
+      }
+    }
+  }
+
 }
+
+new adminController();
 
 module.exports = router;
