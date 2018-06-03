06a4c6245a2d5a261c5e2b69cfdc543ef375bd0e
rizkisadewa <rizkykysadewa@gmail.com>
2018-06-03 13:49:07 +0800

    database : KoneksiBasisData

--- config/database.js
+++ config/database.js
@@ -1,4 +1,15 @@
-module.exports = {
-  database: 'mongodb://localhost:27017/destbankdb',
-  secret: 'yoursecret'
+class KoneksiBasisData {
+  constructor() {
+    this.execute();
+  }
+
+  execute(){
+    module.exports = {
+      database: 'mongodb://localhost:27017/destbankdb',
+      secret: 'yoursecret'
+    }
+  }
+
 }
+
+new KoneksiBasisData();
